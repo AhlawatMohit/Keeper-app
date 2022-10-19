ddb20f0ac18b6c128eb84a30507c76bdab516a15
Mohit Ahlawat <ahlawat@Mohits-MacBook-Air.local>
2022-10-19 22:42:14 +0530

    completed

---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import BorderColorRoundedIcon from '@mui/icons-material/BorderColorRounded';
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <BorderColorRoundedIcon />
+        Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
